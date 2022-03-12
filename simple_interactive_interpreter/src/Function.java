60bf76d21b081d72e670911e5a2e0166f7a15ca4
caropina <carolinasgro960@gmail.com>
2022-03-12 22:48:54 +0100

    Created Function class

---
+++ simple_interactive_interpreter/src/Function.java
@@ -0,0 +1,9 @@
+public class Function {
+  List<String> parameters;
+  List<String> expression;
+
+  public Function (List<String> parameters, List<String> expression) {
+    this.parameters = parameters;
+    this.expression = expression;
+  }
+}
